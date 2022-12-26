fb70d1efc3e2feef3e538cb47fb8274675675269
CaioHSCruz <caiohscruz@hotmail.com>
2022-12-26 15:58:01 -0300

    Add GRoomClient

--- gRoomServer/Services/GRoomService.cs
+++ gRoomServer/Services/GRoomService.cs
@@ -14,6 +14,7 @@ public class GRoomService : GRoom.GRoomBase
     public override Task<RoomRegistrationResponse> RegisterToRoom(RoomRegistrationRequest request, ServerCallContext context)
     {
         _logger.LogInformation("Service called...");
+        _logger.LogInformation($"Room Name: {request.RoomName}");
         var rnd = new Random();
         var roomNum = rnd.Next(1, 100);
         _logger.LogInformation($"Room no. {roomNum}");
